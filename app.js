3e58711d47f0a9cb67befd10c7a3d8af581e2041
SHUBHO CHAKRABORTY <shubho1chakraborty1@gmail.com>
2021-12-31 01:13:12 +0530

    Tested and Final

--- app.js
+++ app.js
@@ -2,14 +2,29 @@ const cheerio = require('cheerio');
 const puppeteer = require('puppeteer');
 const schedule = require('node-schedule');
 const db = require(__dirname + '/dbManager');
+const urls = require(__dirname + '/urls');
+
+//Increase Event Listner MaxLimit (Just in case needed someday):
+const EventEmitter = require('events');
+class MyEmitter extends EventEmitter {}
+const myEmitter = new MyEmitter();
+// increase the limit
+myEmitter.setMaxListeners(15);
 
 const url = 'https://www.jiomart.com';
-const categories = [];
+const categories = urls.getUrls();
 
 // Get All Categories from the website:
 async function getCategories(){
-    let page = await configureBrowser(url);
-    await addCategories(page);
+
+// For URL automation:
+    // let page = await configureBrowser(url);
+    // await addCategories(page);
+    
+    // Loop through each category for extraction:
+    for(let category of categories){
+        getItems(category.categoryLink, category.categoryName);
+    }
 }
 
 // Collect all the Name's of Categories along with their Url's
@@ -25,15 +40,10 @@ async function addCategories(page) {
             categoryLink
         })
     });
-    // Loop through each category for extraction:
-    for(let category of categories){
-        getItems(category.categoryLink, category.categoryName);
-    }
 }
 
 // Get all the Items from the current Page:
 async function getItems(link, name){
-    console.log("Here");
     let obj = {
         loop: true,
         url: link,
@@ -42,19 +52,19 @@ async function getItems(link, name){
     }
     while(obj.loop){
         let page = await configureBrowser(obj.url);
-        obj = await checkPrice(page, obj);
+        await checkPrice(page, obj);
     }
     // Add all the Items in the DataBase:
+    console.log(obj.categoryItems.length);
     db.addToDB(name, obj.categoryItems);
 }
 
 // Collect all the name and price of each product for the current category:
 async function checkPrice(page, OBJ) {
-    const Items = [];
     await page.reload();
     let html = await page.evaluate(() => document.body.innerHTML);
     const $ = cheerio.load(html);
-    $('.cat-item').each(function() {
+    $('.col-md-3').each(function() {
         let price = $(this).find('.price-box #final_price').text();
         let name = $(this).find('.clsgetname').text();
         OBJ.categoryItems.push({
@@ -63,13 +73,12 @@ async function checkPrice(page, OBJ) {
         })
     });
     console.log(OBJ.pageNumber + "::::::::::::::::::::");
-    if($('.pages .next').text() === 'NEXT' && OBJ.pageNumber < 100){
-        OBJ.url = $('.pages .next').find("a").attr('href');
+    if($('.pages .next').text() === 'NEXT' && OBJ.pageNumber < 2){
+        OBJ.url = $('.next').find("a").attr('href');
         OBJ.pageNumber += 1;
     }
     else
         OBJ.loop = false;
-    return OBJ;
 }
 
 async function configureBrowser(URL) {
@@ -85,6 +94,6 @@ schedule.scheduleJob('0 0 10 * * *', getCategories);
 
 //RUN:
 getCategories();
-// getItems('https://www.jiomart.com/c/groceries/fruits-vegetables/219/page/1', 'FruitsVegetables', 1);
+//getItems('https://www.jiomart.com/c/groceries/fruits-vegetables/219/page/1', 'FruitsVegetables', 1);
 // getItems('https://www.jiomart.com/c/groceries/dairy-bakery/61', 'DairyBakery', 1);
 // getItems('https://www.jiomart.com/c/groceries/premium-fruits/3107', 'PremiumFruits', 1);
